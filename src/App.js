8a3a0ea2003c4c42fd1f16b4ad4052b3589878f7
Kavi919 <kavindyagethmi919@gmail.com>
2024-09-27 14:49:32 +0530

    my-table-app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DataTable from './components/DataTable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+const [data, setData] = useState([]);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
+
+// Fetching data using Axios
+useEffect(() => {
+  const fetchData = async () => {
+    try{
+      const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+      setData(response.data);
+      setLoading(false);
+    }
+    catch (error){
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  fetchData();
+}, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching data: {error.message}</p>;
+
+  return(
+    <div>
+      <h1>User Data Table</h1>
+      <DataTable data={data} />
     </div>
   );
-}
+};
+
 
 export default App;
---
+++ src/components/DataTable.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const DataTable = ({ data }) => {
+  return (
+    <table border="1" cellPadding="10">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+
+export default DataTable;
